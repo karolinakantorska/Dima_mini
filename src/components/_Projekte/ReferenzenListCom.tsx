f2db650043a2d2c1a580449ad8392ecfb9eca444
karolina <karolina.kantorska@gmail.com>
2022-06-22 11:46:15 +0200

    test2

--- src/components/_Projekte/ReferenzenListCom.tsx
+++ src/components/_Projekte/ReferenzenListCom.tsx
@@ -22,7 +22,6 @@ import { ProjectsListType, User } from '../../utils/TS/interface';
 export function ReferenzenListCom(
   { projectsList }: {
     projectsList: ProjectsListType,
-
   }) {
 
 
@@ -31,43 +30,45 @@ export function ReferenzenListCom(
   const isSmall = useResponsive('down', 'sm');
   const gtc = isDesktop ? 'repeat(3, 1fr)' : isSmall ? '1fr' : 'repeat(2, 1fr)';
 
+  if (projectsList) {
+    return (
+      <>
+        <Grid container direction="column" justifyContent="center" spacing={2} >
+          <Grid item>
+            <Box
+              display="grid"
+              gridTemplateColumns={gtc}
+              //justifyItems='stretch'
+              //gridAutoFlow="dense"
+              columnGap="12px"
+              rowGap="20px"
+            >
+              {projectsList.map((project, i) => {
+                const divideIn2 = i % 2 == 0 ? true : false;
+                const divideIn4 = (i + 1) % 4 == 0 ? true : false;
+                const divideIn8 = (i + 1) % 8 == 0 ? true : false;
+                if (project) {
+                  return (
+                    <ProjektCardCom
+                      key={project.id}
+                      project={project}
+                      gridRow={divideIn2 ? '1' : '2'}
+                      big={divideIn4 ? true : false}
+                      //big={false}
+                      rewerseBig={divideIn8 ? true : false}
+                    />
+                  );
+                }
 
-  return (
-    <>
+              })}
+            </Box>
+          </Grid>
+        </Grid >
+      </>
+    );
+  }
+  else {
+    <p>Es gibt momentan keine Projekte in Data Base</p>
+  }
 
-      <Grid container direction="column" justifyContent="center" spacing={2} >
-
-        <Grid item>
-
-          <Box
-            display="grid"
-            gridTemplateColumns={gtc}
-            //justifyItems='stretch'
-            //gridAutoFlow="dense"
-            columnGap="12px"
-            rowGap="20px"
-          >
-            {projectsList.map((project, i) => {
-              const divideIn2 = i % 2 == 0 ? true : false;
-              const divideIn4 = (i + 1) % 4 == 0 ? true : false;
-              const divideIn8 = (i + 1) % 8 == 0 ? true : false;
-              return (
-
-                <ProjektCardCom
-                  key={project.id}
-                  project={project}
-                  gridRow={divideIn2 ? '1' : '2'}
-                  big={divideIn4 ? true : false}
-                  //big={false}
-                  rewerseBig={divideIn8 ? true : false}
-                />
-
-              );
-            })}
-          </Box>
-        </Grid>
-      </Grid >
-
-    </>
-  );
 }
